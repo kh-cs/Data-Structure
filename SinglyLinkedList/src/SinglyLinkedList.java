969a41ad5b2fcf43d117ed0eaf88b1b120cb1b64
Khaled Jalal Al-arashi <77218749+kh-cs@users.noreply.github.com>
2024-05-06 19:39:03 +0300

    Add files via upload

---
+++ SinglyLinkedList/src/SinglyLinkedList.java
@@ -0,0 +1,100 @@
+
+public class SinglyLinkedList<E> {
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0 ;
+    
+    public int size(){
+        return size ;
+    }
+    
+    public boolean isEmpty(){
+        return size==0;
+    }
+    
+    public void add_First(E element){
+       head = new Node<>(element, head);
+       if(size==0)
+           tail=head;
+       size++;
+//       Node<E> newNode = new Node<E>(element , head);
+//       head = newNode;
+    }
+    public void add_Last(E element){
+        Node<E> newNode= new Node<>(element, null);
+        if(size==0){
+            head=newNode;
+        }
+        else{
+            tail.setNext(newNode);
+        }
+        tail = newNode;
+    }
+    public void addOnPosition(E element , int position){
+        Node<E> newNode = new Node<>(element , null);
+        if(position==1){
+            newNode.setNext(head);
+            head=newNode ;
+        }
+        else{
+            Node<E> temp = head ;
+            int count = 1 ;
+            while(count< position-1){
+                temp = temp.getNext();
+                count++;
+            }
+            Node<E> current = temp.getNext();
+            temp.setNext(newNode);
+            newNode.setNext(current);
+        }
+    }
+    public E getFirst(){
+        if(size==0){
+            return null;
+        }
+        return head.getElement();
+    }
+    public E getLast(){
+        if(size==0){
+            return null;
+        }
+        return tail.getElement();
+    }
+    
+    public void display(){
+        Node<E> i = head ;
+        while(i!=null){
+            System.out.println(i.getElement());
+            i = i.getNext();
+        }
+    }
+    
+    private static class Node<E> {
+
+        private E element;
+        private Node<E> next; //link - pointer
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+    }
+}
