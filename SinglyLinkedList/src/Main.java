e113dc81bc27f533dada26905db6727f2d14f30d
Khaled Jalal Al-arashi <77218749+kh-cs@users.noreply.github.com>
2024-05-12 03:07:22 +0300

    Add files via upload

---
+++ SinglyLinkedList/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> s_linkedlist = new SinglyLinkedList<>();
+        s_linkedlist.add_First(1);
+        s_linkedlist.add_First(2);
+        s_linkedlist.add_First(3);
+        s_linkedlist.add_First(4);
+        s_linkedlist.add_First(5);
+        s_linkedlist.display();
+        s_linkedlist.addOnPosition(100, 3);
+        s_linkedlist.removeFirst();
+        System.out.println("============");
+        s_linkedlist.display();
+    }
+}
